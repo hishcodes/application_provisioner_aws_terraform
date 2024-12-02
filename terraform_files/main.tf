2912933927e4fc84a0a5e430e8a92643cf771ae6
hishcodes <hishamkumbra@gmail.com>
2024-12-02 19:38:19 +0530

    sg

--- terraform_files/main.tf
+++ terraform_files/main.tf
@@ -25,28 +25,3 @@ resource "aws_route_table_association" "pub_rt_association" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_security_group" "my_sg" {
-  name        = "ec2 sg"
-  description = "Allow inbound 80,22 and outbound all port"
-  vpc_id      = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
